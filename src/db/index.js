db5534814eccbb4c3c7c6f8206e8f3aa64cafeda
shashikant010 <shashikantyadav0503@gmail.com>
2024-05-07 18:18:18 +0530

    changed mongo url to atlas url

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async()=>{
     
     try {
-        const connection = await mongoose.connect(`mongodb://localhost:27017/jobBoard`)
+        const connection = await mongoose.connect(`mongodb+srv://shashikant:sky9718@cluster0.lbiebv5.mongodb.net/jobBoard`)
         console.log("connected to mongodb successfuly : ",connection.connection.host)
     } catch (error) {
         console.log("MongoDB connection error : ",error)
